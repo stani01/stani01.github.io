47cd471274c2f539e5e75d284f9a4c932e31a7bf
stani01 <s_i0nu7z31@yahoo.com>
2026-03-22 12:26:06 +0200

    added raksha, rearraged ducat instances

--- tracker/js/data/instances.js
+++ tracker/js/data/instances.js
@@ -4,10 +4,11 @@
 var DUCAT_INSTANCES = [
     { id: 'ara-infernalia', name: 'Ara Infernalia (normal)', maxRuns: 4 },
     { id: 'beninerk', name: "Beninerk's Manor (normal)", maxRuns: 4 },
-    { id: 'benshmundir', name: 'Benshmundir Storm Temple (normal)', maxRuns: 4 },
-    { id: 'dokkaebi', name: 'Dokkaebi Realm', maxRuns: 4 },
+    { id: 'stella', name: 'Stella Dev. Laboratory (normal)', maxRuns: 4 },
     { id: 'makarna', name: 'Makarna of Bitterness (difficult)', maxRuns: 4 },
     { id: 'prometun', name: "Prometun's Workshop (difficult)", maxRuns: 4 },
-    { id: 'senekta', name: 'Senekta', maxRuns: 2 },
-    { id: 'stella', name: 'Stella Development Laboratory (normal)', maxRuns: 4 }
+    { id: 'benshmundir', name: 'Benshmundir S. Temple (normal)', maxRuns: 4 },
+    { id: 'dokkaebi', name: 'Dokkaebi Realm', maxRuns: 4 },
+    { id: 'raksha', name: 'Burning Blood Fortress (S - rank)', maxRuns: 3 },
+    { id: 'senekta', name: 'Senekta', maxRuns: 2 }
 ];
